489c590123323ac8fc5ca8f59d72d716716fc672
hameed2k3 <adhameed2k3@gmail.com>
2026-02-10 15:04:15 +0530

    fix the attatchements

--- server/server.js
+++ server/server.js
@@ -42,7 +42,8 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Test Endpoint for DB Connection
 app.get('/api/test', (req, res) => {
