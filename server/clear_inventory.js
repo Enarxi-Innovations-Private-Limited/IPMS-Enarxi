e7154f744598cef03d5d50631c4caf78920ecd21
hameed2k3 <adhameed2k3@gmail.com>
2026-06-18 12:15:05 +0530

    chore: align repository hygiene for production sync

--- server/clear_inventory.js
+++
@@ -1,7 +0,0 @@
-const { clearInventoryDB, parseArgs } = require('./clear_inventory_db');
-
-console.warn('[DEPRECATED] Use `node server/clear_inventory_db.js ...` instead of `node server/clear_inventory.js ...`.');
-
-if (require.main === module) {
-    clearInventoryDB(parseArgs(process.argv.slice(2)));
-}
